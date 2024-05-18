4eb0c3d503b1e2c57d2b515c0a4a9581daddb3f5
FrancisMauhay <francisemmanuel.mauhay@benilde.edu.ph>
2024-05-18 12:57:13 +0800

    Milestone 2
    Submission for Milestone 2 Mauhay

--- SDLTemplate/SDLTemplate/Player.cpp
+++ SDLTemplate/SDLTemplate/Player.cpp
@@ -1,4 +1,6 @@
 #include "Player.h"
+#include "Scene.h"
+#include <algorithm>
 
 void Player::start()
 {
@@ -8,12 +10,72 @@ void Player::start()
 	width = 0;
 	height = 0;
 	speed = 2;
+	currentReloadTime = 0;
+	reloadTime = 20;
+	currentSpecialReloadTime = 0;
+	specialReloadTime = 60;
 	SDL_QueryTexture(texture, NULL, NULL, &width, &height);
+	fire = SoundManager::loadSound("sound/334227__jradcoolness__laser.ogg");
 }
 
 void Player::update()
 {
 	setSpeed();
+	move();
+
+	if (currentReloadTime > 0)
+	{
+		currentReloadTime--;
+	}
+
+	if (currentSpecialReloadTime > 0)
+	{
+		currentSpecialReloadTime--;
+	}
+
+	if (app.keyboard[SDL_SCANCODE_SPACE] && currentReloadTime == 0)
+	{
+		shoot();
+	}
+	
+	if (app.keyboard[SDL_SCANCODE_G] && currentSpecialReloadTime == 0)
+	{
+		specialShoot();
+	}
+
+	// had asked chatgpt what is a more optimal way of memory management and it suggested using remove_if
+	auto it = std::remove_if(bullets.begin(), bullets.end(), [](Bullet* bullet) 
+		{
+		if (bullet->getPositionX() > SCREEN_WIDTH) 
+		{
+			delete bullet;
+			return true;
+		}
+		return false;
+		});
+
+	bullets.erase(it, bullets.end());
+}
+
+void Player::draw()
+{
+	blit(texture, x, y);
+}
+
+void Player::setSpeed()
+{
+	if (app.keyboard[SDL_SCANCODE_LSHIFT] && speed == 2)
+	{
+		speed = 5;
+	}
+	else if (app.keyboard[SDL_SCANCODE_BACKSPACE] && speed == 5)
+	{
+		speed = 2;
+	}
+}
+
+void Player::move()
+{
 	if (app.keyboard[SDL_SCANCODE_W])
 	{
 		y -= speed;
@@ -32,19 +94,28 @@ void Player::update()
 	}
 }
 
-void Player::draw()
+void Player::shoot()
 {
-	blit(texture, x, y);
+	SoundManager::playSound(fire);
+	Bullet* bullet = new Bullet(x + width ,y - 2 + height/2 ,1,0,10);
+	bullets.push_back(bullet);
+	getScene()->addGameObject(bullet);
+	bullet->start();
+
+	currentReloadTime = reloadTime;
 }
 
-void Player::setSpeed()
+void Player::specialShoot()
 {
-	if (app.keyboard[SDL_SCANCODE_LSHIFT] && speed == 2)
-	{
-		speed = 5;
-	}
-	else if (app.keyboard[SDL_SCANCODE_BACKSPACE] && speed == 5)
-	{
-		speed = 2;
-	}
+	SoundManager::playSound(fire);
+	Bullet* specialBullet1 = new Bullet(x, y + 3, 1, 0, 10);
+	Bullet* specialBullet2 = new Bullet(x, y - 2 + height, 1, 0, 10);
+	getScene()->addGameObject(specialBullet1);
+	getScene()->addGameObject(specialBullet2);
+	bullets.push_back(specialBullet1);
+	bullets.push_back(specialBullet2);
+	specialBullet1->start();
+	specialBullet2->start();
+
+	currentSpecialReloadTime = specialReloadTime;
 }
--- SDLTemplate/SDLTemplate/Player.h
+++ SDLTemplate/SDLTemplate/Player.h
@@ -2,6 +2,11 @@
 #include "GameObject.h"
 #include "common.h"
 #include "draw.h"
+#include "SoundManager.h"
+#include "Bullet.h"
+#include <vector>
+
+using namespace std;
 
 class Player :
     public GameObject
@@ -11,12 +16,21 @@ public:
     void update();
     void draw();
     void setSpeed();
+    void move();
+    void shoot();
+    void specialShoot();
 private:
     SDL_Texture* texture;
+    Mix_Chunk* fire;
     int x;
     int y;
     int width;
     int height;
     int speed;
+    float reloadTime;
+    float currentReloadTime;
+    float specialReloadTime;
+    float currentSpecialReloadTime;
+    vector<Bullet*> bullets;
 };
 
